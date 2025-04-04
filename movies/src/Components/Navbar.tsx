fdfceeee14a01423e6e911473655cd373e9d4897
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-04 11:41:59 +0300

    first commit

---
+++ movies/src/Components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import styles from "../styles/Navbar.module.scss";
+
+import logo from "../assets/logo.jpg";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <nav
+        className={`${styles.navbar} ${isOpen ? styles.navbar_responsive : ""}`}
+      >
+        <div className={styles.logo}>
+          <img src={logo} alt="Logo" />
+        </div>
+        <ul className={styles.nav_list}>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">People</a>
+          </li>
+        </ul>
+        {isOpen ? (
+          <IoClose className={styles.navmenu_icon} onClick={toggleNavbar} />
+        ) : (
+          <RxHamburgerMenu
+            className={styles.navmenu_icon}
+            onClick={toggleNavbar}
+          />
+        )}
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
