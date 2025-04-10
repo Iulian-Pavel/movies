de8ea32670e4c0375c10982b04551850581c4026
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-10 11:19:09 +0300

    imported the api key from the constants file

--- movies/src/services/tmdb.tsx
+++ movies/src/services/tmdb.tsx
@@ -1,7 +1,7 @@
 import Axios from "axios";
+import { API_KEY } from "../constants";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = "4c6e20a013dbfc2ec4f9c3f78598699f";
 
 
 //Create Axios instance
