cf503c206bf2a7d8ab316ca7da16fef38af8dde3
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-10 11:20:26 +0300

    added dynamic routes for each movie and a route to the home page

--- movies/src/App.tsx
+++ movies/src/App.tsx
@@ -1,13 +1,19 @@
 import Navbar from "./Components/Navbar";
+import MovieDetails from "./pages/MovieDetails";
 import PopularMovies from "./pages/PopularMovies";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <PopularMovies />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<PopularMovies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
