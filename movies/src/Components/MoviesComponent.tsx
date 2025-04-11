7c8baba89019e1349ad40368830ea01d3ed08221
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-11 13:25:45 +0300

    grouped imports for better consistency

--- movies/src/Components/MoviesComponent.tsx
+++ movies/src/Components/MoviesComponent.tsx
@@ -1,6 +1,6 @@
+import { Link } from "react-router-dom";
 import { POSTER_PATH } from "../constants";
 import styles from "../styles/MovieCard.module.scss";
-import { Link } from "react-router-dom";
 
 interface Props {
   id: number,
@@ -20,7 +20,7 @@ function MoviesComponent({
   return (
         <div className={styles.movie_card}>
           <div className={styles.movie_card_poster}>
-            <img src={`${POSTER_PATH}${poster_path}`} alt="placeholder" />
+            <img src={`${POSTER_PATH}${poster_path}`} width={"100%"} height={"100%"} alt="placeholder" />
           </div>
           <div className={styles.movie_card_details}>
             <button><Link to={`/movies/${id}`}>{title}</Link></button>
