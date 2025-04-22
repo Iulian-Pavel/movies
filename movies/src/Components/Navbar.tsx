eafe6b2382eda20a2e65a039e5f8573401c5d49a
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-22 08:05:33 +0300

    new logo

--- movies/src/Components/Navbar.tsx
+++ movies/src/Components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoClose } from "react-icons/io5";
 import styles from "../styles/Navbar.module.scss";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/logo.png";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
