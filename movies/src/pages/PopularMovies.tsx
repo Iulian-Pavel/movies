53aee7b37c3d4309ed1270c30f78d283ef8d312a
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-22 09:13:27 +0300

    migrated to redux toolkit

--- movies/src/pages/PopularMovies.tsx
+++ movies/src/pages/PopularMovies.tsx
@@ -1,27 +1,23 @@
 import { useEffect, useState } from "react";
+import { useDispatch, UseSelector, useSelector } from "react-redux";
+import { fetchPopularMoviesThunk } from "../store/movieSlice";
+import { RootState, AppDispatch } from "../store"
 import { fetchPopularMovies, Movie } from "../services/tmdb";
 import MoviesComponent from "../Components/MoviesComponent";
 import styles from "../styles/MovieCard.module.scss";
 import SearchBar, { SearchTypes } from "../Components/SearchBar";
 
 function PopularMovies() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(true);
+  // const [movies, setMovies] = useState<Movie[]>([]);
+  // const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch<AppDispatch>();
+  const movies = useSelector((state: RootState) => state.movies.popular);
+  const loading = useSelector((state: RootState) => state.movies.loading);
 
   useEffect(() => {
-    const getMovies = async () => {
-      try {
-        const data = await fetchPopularMovies();
-        setMovies(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getMovies();
-  }, []);
+    dispatch(fetchPopularMoviesThunk());
+  }, [dispatch]);
 
   if (loading) return <p>Loading...</p>;
 
