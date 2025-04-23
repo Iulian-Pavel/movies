3ea874bcb95b125e46fe793236be72d35adbf3fa
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-23 14:48:47 +0300

    used person component in the people page

---
+++ movies/src/pages/PeoplePage.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import {
+  selectPeople,
+  selectPeopleLoading,
+  selectPeopleError,
+} from "../selectors/peopleSelectors";
+import { fetchPeopleThunk } from "../store/personSlice";
+import { AppDispatch } from "../store";
+import { useDispatch, useSelector } from "react-redux";
+import PersonComponent from "../Components/PersonComponent";
+import styles from '../styles/PersonComponent.module.scss'
+
+function PeoplePage() {
+  const dispatch = useDispatch<AppDispatch>();
+  const people = useSelector(selectPeople);
+  const loading = useSelector(selectPeopleLoading);
+  const error = useSelector(selectPeopleError);
+
+  useEffect(() => {
+    dispatch(fetchPeopleThunk());
+  }, [dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (!people) return <p>{error}</p>;
+  return (
+    <>
+    <div className={styles.person_container}>
+        {people.map((person) => (
+          <PersonComponent 
+          key={person.id}
+            id={person.id}
+            profile_path={person.profile_path}
+            name={person.name}
+            known_for_department={person.known_for_department}
+            known_for={person.known_for}
+          />
+        ))}
+        </div>
+    </>
+  );
+}
+
+export default PeoplePage;
