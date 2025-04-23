c20c45fb9560c13d9791a1efdcbb7661221b4642
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-23 14:51:16 +0300

    added person reducer

--- movies/src/store/index.ts
+++ movies/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from './movieSlice';
+import personReducer from './personSlice';
 
 export const store = configureStore({
     reducer: {
-        movies: movieReducer
+        movies: movieReducer,
+        people: personReducer
     }
 });
 
