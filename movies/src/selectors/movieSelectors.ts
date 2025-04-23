c3755358ef3b17f6c26524c9b5e071e6d7912457
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-23 14:50:22 +0300

    added the error selector

--- movies/src/selectors/movieSelectors.ts
+++ movies/src/selectors/movieSelectors.ts
@@ -2,3 +2,4 @@ import { RootState } from "../store";
 
 export const selectPopularMovies = (state: RootState) => state.movies.popular;
 export const selectMoviesLoading = (state: RootState) => state.movies.loading;
+export const selectMoviesError = (state: RootState) => state.movies.error;
