7bef86a0dd9c2091f6f78cfc008d40c51e1900cf
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-24 13:36:29 +0300

    added try catch blocks

--- movies/src/store/personSlice.ts
+++ movies/src/store/personSlice.ts
@@ -1,47 +1,53 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchPeople, Person } from "../services/tmdb";
+import { fetchPeople } from "../services/tmdb";
+import { Person } from "../services/tmdb.type";
 
 interface PeopleState {
-    people: Person[];
-    loading: boolean;
-    error: string | null;
-    id: number
+  people: Person[];
+  loading: boolean;
+  error: string | null;
+  id: number;
 }
 
 const initialState: PeopleState = {
-    people: [],
-    loading: false,
-    error: null,
-    id: 0
-}
+  people: [],
+  loading: false,
+  error: null,
+  id: 0,
+};
 
 export const fetchPeopleThunk = createAsyncThunk<Person[]>(
-    'popular people',
-    async () => {
-        const people = await fetchPeople();
-        return people;
+  "popular people",
+  async () => {
+    try {
+      const people = await fetchPeople();
+      return people;
+    } catch (error: any) {
+      console.log(error.message);
+      return error.message;
     }
-)
+  }
+);
 
 const personSlice = createSlice({
-    name: 'people',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-        .addCase(fetchPeopleThunk.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-        })
-        .addCase(fetchPeopleThunk.fulfilled, (state, action) => {
-            state.loading = false;
-            state.people = action.payload;
-        })
-        .addCase(fetchPeopleThunk.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.error.message || "Failed to fetch people"
-        })
-    },
+  name: "people",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPeopleThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchPeopleThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.people = action.payload;
+      })
+      .addCase(fetchPeopleThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to fetch people";
+      });
+  },
 });
 
 export default personSlice.reducer;
