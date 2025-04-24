da3e6fed97ba8d13eb615dde36daf3d9e1783050
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-04-24 13:40:04 +0300

    destructured props to prevent empty renders

--- movies/src/Components/MoviesComponent.tsx
+++ movies/src/Components/MoviesComponent.tsx
@@ -1,33 +1,39 @@
 import { Link } from "react-router-dom";
 import { POSTER_PATH } from "../constants";
 import styles from "../styles/MovieCard.module.scss";
+import { Movie } from "../services/tmdb.type";
 
-interface Props {
-  id: number,
-  title: string;
-  release_date: string;
-  poster_path: string;
-  popularity: number;
-}
+type Props = {
+  movie: Movie;
+};
 
-function MoviesComponent({
-  id,
-  title,
-  release_date,
-  poster_path,
-  popularity,
-}: Props) {
+function MoviesComponent({ movie }: Props) {
+  const {
+    id = 0,
+    title = "Unknown",
+    release_date = "Unreleased",
+    poster_path = "",
+    popularity = "",
+  } = movie;
+
   return (
-        <div className={styles.movie_card}>
-          <div className={styles.movie_card_poster}>
-            <img src={`${POSTER_PATH}${poster_path}`} width="100%" height="100%" alt="placeholder" />
-          </div>
-          <div className={styles.movie_card_details}>
-            <button><Link to={`/movies/${id}`}>{title}</Link></button>
-            <p>{release_date}</p>
-            <p>{popularity}</p>
-          </div>
-        </div>
+    <div className={styles.movie_card}>
+      <div className={styles.movie_card_poster}>
+        <img
+          src={`${POSTER_PATH}${poster_path}`}
+          width="100%"
+          height="100%"
+          alt="placeholder"
+        />
+      </div>
+      <div className={styles.movie_card_details}>
+        <button>
+          <Link to={`/movies/${id}`}>{title}</Link>
+        </button>
+        <p>{release_date}</p>
+        <p>{popularity}</p>
+      </div>
+    </div>
   );
 }
 
